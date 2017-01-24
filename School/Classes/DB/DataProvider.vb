01ed290965e0a44c1ea56e35375225b27b3d9375
taimoor <taimoorali@gmail.com>
2017-01-24 21:18:08 +0500

    Insert Delete and Update Ajax
    Code for Insert Delete and Update by using Ajax And stored Procedure in
    Gridview

--- School/Classes/DB/DataProvider.vb
+++ School/Classes/DB/DataProvider.vb
@@ -2,7 +2,6 @@
 Imports Microsoft.Practices.EnterpriseLibrary.Common
 Public Class DataProvider
 
-
     Sub New()
     End Sub
 
@@ -16,5 +15,15 @@ Public Class DataProvider
         objDatabase = DatabaseFactory.CreateDatabase()
         Return CType(objDatabase.ExecuteDataSet("P_Student_IU", Obj.ST_ID, Obj.Name), DataSet).Tables(0).Rows(0).Item(0)
     End Function
+    Public Shared Function P_Student_GetByST_ID(ST_ID As Integer) As DataRow
+        Dim objDatabase As Database
+        objDatabase = DatabaseFactory.CreateDatabase()
+        Return CType(objDatabase.ExecuteDataSet("P_Student_GetByST_ID", ST_ID), DataSet).Tables(0).Rows(0)
+    End Function
+    Public Shared Sub P_Student_Delete(ST_ID As Integer)
+        Dim objDatabase As Database
+        objDatabase = DatabaseFactory.CreateDatabase()
+        objDatabase.ExecuteNonQuery("P_Student_Delete", ST_ID)
+    End Sub
 
 End Class
