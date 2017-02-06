e88f8b5206ca58de47d199a9099295a06c4dd59d
taimoorg <taimoorali504@gmail.com>
2017-02-06 23:01:35 +0500

    Update StudentInfo.vb

--- Info/StudentInfo.vb
+++ Info/StudentInfo.vb
@@ -2,12 +2,14 @@
 Public Class StudentInfo
     Public Property ST_ID As Integer
     Public Property Name As String
+    Public Property Father_Name As String
     Sub New()
 
     End Sub
     Sub New(StdID As Integer, stdName As String)
         ST_ID = StdID
         Name = stdName
+        Father_Name = fatherName
 
     End Sub
 
